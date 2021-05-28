27f858f0765d2d54fb904fcd1040793cbb00fba4
gazda <74616786+JKGplay@users.noreply.github.com>
2021-05-28 17:15:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const div1 = document.getElementById("js");
 
-
-//if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    // some code.. 
-//    }
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    div1.innerHTML = "JavaScript działa na telefonach";
+} else {
+    div1.innerHTML = "JavaScript działa na kompach";
+}
